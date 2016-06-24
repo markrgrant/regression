93fdebf707e504ce57e09fc7cc83b0268315839d
Mark Grant <markrgrant@yahoo.com>
2016-06-24 11:27:08 -0700

    add examples

---
+++ app/HMatrixGradient.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+
+import Stat
+import House
+
+import Data.Csv
+import GHC.Exts (sortWith)
+import Regression.HMatrix
+import System.Environment (getArgs)
+import qualified Data.ByteString.Lazy as B
+import qualified Data.Vector as V
+
+
+main :: IO ()
+main = do
+    [filename] <- getArgs
+    fileData <- B.readFile filename
+    let csv = decodeByName fileData :: Either String (Header, V.Vector House)
+    case csv of
+        Left err -> putStrLn $ "error reading " ++ filename
+        Right (header, houses) ->  do
+            let inputs = V.toList houses
+                output = ("price", price)
+                features = [("w0", const 1), ("sqft_living", sqft_living)]
+                fmat = create_features features inputs
+                e = 3.08e8
+                n = 0.000000000002
+                initial_weights = create_weights fmat [0,0..0]
+                optimizer = gradient_descent e n initial_weights
+                model = create_model inputs features output optimizer
+                weights = model_weights model
+            putStrLn $ "model weights:\n" ++ show (model_weights model)
+            putStrLn $ "model rss: " ++ show (model_rss model)
---
+++ app/HMatrixNewtons.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+
+import Stat
+import House
+
+import Data.Csv
+import GHC.Exts (sortWith)
+import Regression.HMatrix
+import System.Environment (getArgs)
+import qualified Data.ByteString.Lazy as B
+import qualified Data.Vector as V
+
+
+main :: IO ()
+main = do
+    [filename] <- getArgs
+    fileData <- B.readFile filename
+    let csv = decodeByName fileData :: Either String (Header, V.Vector House)
+    case csv of
+        Left err -> putStrLn $ "error reading " ++ filename
+        Right (header, houses) ->  do
+            let inputs = V.toList houses
+                output = ("price", price)
+                features = [("w0", const 1), ("sqft_living", sqft_living)]
+                fmat = create_features features inputs
+                optimizer = newtons_method 
+                model = create_model inputs features output optimizer
+                weights = model_weights model
+            putStrLn $ "model weights:\n" ++ show (model_weights model)
+            putStrLn $ "model rss: " ++ show (model_rss model)
